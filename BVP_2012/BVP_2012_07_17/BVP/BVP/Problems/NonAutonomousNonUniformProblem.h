2cd55129e64a14745a161bb1769f4f116b69b3c7
DESKTOP-9KP0AJP\Dragunov <dragunovdenis@gmail.com>
2018-07-21 19:16:32 +0300

    Testing framework and tests for non-autonomous non-uniform IVPs, BVPs

---
+++ BVP_2012/BVP_2012_07_17/BVP/BVP/Problems/NonAutonomousNonUniformProblem.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "ProblemNonAutonomousAbstract.h"
+#include "../Utils/AuxUtils.h"
+#include <math.h>
+
+template <class T>
+class NonAutonomousNonUniformProblem : public ProblemNonAutonomousAbstract<T>
+{
+private:
+	T _alpha;
+protected:
+
+	///Nonlineariti for the Troesch problem
+	virtual T Nonlin(const T& u, const T& x) override
+	{
+		return exp(2*u + cos(_alpha*x)) - _alpha*_alpha;
+	}
+
+	///Derivative of nonlinearity (with respect to u)
+	virtual T dNonlinDu(const T& u, const T& x) override
+	{
+		return 2*exp(2*u + cos(_alpha*x));
+	}
+	
+	///Derivative of nonlinearity (with respect to x)
+	virtual T dNonlinDx(const T& u, const T& x) override
+	{
+		return -_alpha*sin(_alpha*x)*exp(2*u + cos(_alpha*x));
+	}
+
+	///Second derivative of nonlinearity
+	virtual T ddNonlinDuDu(const T& u, const T& x) override
+	{
+		return 4*exp(2*u + cos(_alpha*x));;
+	}
+
+	///Second derivative of nonlinearity
+	virtual T ddNonlinDuDx(const T& u, const T& x) override
+	{
+		return -2*_alpha*sin(_alpha*x)*exp(2*u + cos(_alpha*x));
+	}
+
+	///Second derivative of nonlinearity
+	virtual T ddNonlinDxDx(const T& u, const T& x) override
+	{
+		return _alpha*_alpha*(auxutils::sqr(sin(_alpha*x)) - cos(_alpha*x))*exp(2*u + cos(_alpha*x));
+	}
+
+	///Right hand side function (non-uniformity term)
+	virtual T Phi(const T& x) override
+	{
+		return -exp(2*sin(_alpha*x) + cos(_alpha*x))*sin(_alpha*x);
+	}
+
+	///The first order derivative of the non-unoformity term
+	virtual T dPhi(const T& x)  override
+	{
+		T t1 = _alpha * x;
+		T t2 = cos(t1);
+		T t5 = sin(t1);
+		T t10 = exp(2 * t5 + t2);
+		return -(2 * _alpha * t2 - _alpha * t5) * t10 * t5 - t10 * _alpha * t2;
+
+	}
+
+	///The second order derivative of the non-unoformity term
+	virtual T ddPhi(const T& x)  override
+	{
+		T t1 = _alpha * _alpha;
+		T t2 = _alpha * x;
+		T t3 = sin(t2);
+		T t6 = cos(t2);
+		T t11 = exp(2 * t3 + t6);
+		T t14 = _alpha * t6;
+		T t17 = -_alpha * t3 + 2 * t14;
+		T t18 = t17 * t17;
+		return -(-2 * t1 * t3 - t1 * t6) * t11 * t3 - t18 * t11 * t3 - 2 * t17 * t11 * t14 + t11 * t1 * t3;
+
+	}
+
+public:
+
+	NonAutonomousNonUniformProblem(const T& alpha)
+	{
+		_alpha = alpha;
+	}
+};
--- BVP_2012/BVP_2012_07_17/BVP/GeneralTest/NonUniformProblemTest.cpp
+++ BVP_2012/BVP_2012_07_17/BVP/GeneralTest/NonUniformProblemTest.cpp
@@ -8,6 +8,7 @@
 #include "../BVP/FunctionApproximation/InitialCondition.h"
 #include "../BVP/Utils/AuxUtils.h"
 #include "../BVP/Problems/AutonomousNonUniformProblem.h"
+#include "../BVP/Problems/NonAutonomousNonUniformProblem.h"
 #include "..\BVP\Cannon\HybridCannon.h"
 #include "..\BVP\ShootingSimple\BisectionComponent.h"
 #include "..\BVP\MultipleShooting\HybridMultipleShootingComponent.h"
@@ -22,21 +23,36 @@ namespace GeneralTest
 	TEST_CLASS(NonUniformProblemTest)
 	{
 	private:
-		std::vector<InitCondition<double>> GetGybridShootingResultForward(const double alpha, const double h)
+		template <class T>
+		std::vector<InitCondition<T>> GetGybridShootingResultForward(const T alpha, const T h, const bool nonAutonomousProblem)
 		{
-			 AutonomousNonUniformProblem<double> problem(alpha);
-			 HybridCannon<double> thc(problem, h, 10*std::numeric_limits<double>::epsilon());
+			ProblemAbstract<T>* problem; 
+			if (nonAutonomousProblem)  
+				problem = new NonAutonomousNonUniformProblem<T>(alpha); 
+			else 
+				problem = new AutonomousNonUniformProblem<T>(alpha);
 
-			 auto result = thc.Shoot(1, 4, sin(alpha), 10, alpha*cos(alpha));
-			 return thc.GetKnotVectorStreight();
+			HybridCannon<T> thc(*problem, h, 10*std::numeric_limits<T>::epsilon());
+
+			auto result = thc.Shoot(1, 4, sin(alpha), 10, alpha*cos(alpha));
+
+			delete problem;
+			return thc.GetKnotVectorStreight();
 		}
 
-		std::vector<InitCondition<double>> GetGybridShootingResultBackward(const double alpha, const double h)
+		template <class T>
+		std::vector<InitCondition<T>> GetGybridShootingResultBackward(const T alpha, const T h, const bool nonAutonomousProblem)
 		{
-			 AutonomousNonUniformProblem<double> problem(alpha);
-			 HybridCannon<double> thc(problem, h, 10*std::numeric_limits<double>::epsilon());
+			ProblemAbstract<T>* problem; 
+			if (nonAutonomousProblem)  
+				problem = new NonAutonomousNonUniformProblem<T>(alpha); 
+			else 
+				problem = new AutonomousNonUniformProblem<T>(alpha);
+
+			 HybridCannon<T> thc(*problem, h, 10*std::numeric_limits<T>::epsilon());
 
 			 auto result = thc.Shoot(4, 1, sin(4*alpha), 10, alpha*cos(4*alpha));
+			 delete problem;
 			 return thc.GetKnotVectorStreight();
 		}
 
@@ -65,7 +81,7 @@ namespace GeneralTest
 		}
 
 		template <class T>
-		std::vector<InitCondition<T>> GetHybridBisectionResult(const T alpha, const T h)
+		std::vector<InitCondition<T>> GetHybridBisectionResult(const T alpha, const T h, const bool nonAutonomousProblem)
 		{
 			 T argStart = T(1);
 			 T uStart = sin(alpha*argStart);
@@ -73,19 +89,48 @@ namespace GeneralTest
 			 T uTarget = sin(argTarget*alpha);
 			 T duLeft = alpha*cos(alpha*argStart) - T(0.1);
 			 T duRight = alpha*cos(alpha*argStart) + T(0.1);
-			 AutonomousNonUniformProblem<T> problem(alpha);
+
+			ProblemAbstract<T>* problem; 
+			if (nonAutonomousProblem)  
+				problem = new NonAutonomousNonUniformProblem<T>(alpha); 
+			else 
+				problem = new AutonomousNonUniformProblem<T>(alpha);
 
 			 std::function<bool(const InitCondition<T>&)> checkFunc = 
 				 [](const InitCondition<T>& ic) { return (abs(ic.Value) < 10) && (abs(ic.Argument) < 4); };
 
-			 HybridCannon<T> cannon(problem, h, 10*std::numeric_limits<T>::epsilon(), checkFunc);
+			 HybridCannon<T> cannon(*problem, h, 10*std::numeric_limits<T>::epsilon(), checkFunc);
 
 			 std::function<int(const InitCondition<T>&)> evalFunc = 
 				 [=](const InitCondition<T>& ic) { return sgn(ic.Value - uTarget); };
 			 BisectionComponent<T> bc(cannon);
 			 Assert::IsTrue(bc.DerivativeBisectionGen(argStart, argTarget, uStart, uTarget, duLeft, duRight, evalFunc));
 
+			 delete problem;
 			 return cannon.GetKnotVectorStreight();
+		}
+
+		template <class T>
+		std::vector<InitCondition<T>> GetHybridMultipleShootingResult(const T alpha, const T h, const std::vector<InitCondition<T>> init_guess,
+			const bool nonAutonomousProblem) 
+		{
+			ProblemAbstract<T>* problem; 
+			if (nonAutonomousProblem)  
+				problem = new NonAutonomousNonUniformProblem<T>(alpha); 
+			else 
+				problem = new AutonomousNonUniformProblem<T>(alpha);
+
+			 HybridMultipleShootingComponent<numTypeMp> HMSComp(*problem);
+
+			 bool succeeded;
+			 std::vector<InitCondition<numTypeMp>> result = HMSComp.Run(init_guess, h, succeeded);
+
+			 Assert::IsTrue(succeeded, Message("Hybrid multiple shooting failed."));
+
+			 Assert::IsTrue(CheckQuadraticConvergenceOfNewtonMethd(HMSComp.GetCorrectionMgnitudes()), Message("Cannot confirm quadratic convergence rate"));
+
+			 delete problem;
+			 return result;
 		}
 
 		template <class T>
@@ -95,13 +140,35 @@ namespace GeneralTest
 			for (size_t index = successiveCorrections.size() - 1; index > 0; index--)
 			{
 				T prevCorrectionSquared = auxutils::sqr(successiveCorrections[index - 1]);
-				if (2* prevCorrectionSquared >= successiveCorrections[index])
+				if (3* prevCorrectionSquared >= successiveCorrections[index])
 					numberOfAcceptableCorrections++;
 				else if (index < successiveCorrections.size() - 1) //we can skip the very last correction because it can be not "clear" enough
 					break;
 			}
 
 			return numberOfAcceptableCorrections >= 2;
+		}
+
+		template <class T>
+		void AssertQuadraticOrderOfApproximation(const std::vector<InitCondition<T>>& approximation_h1, const T h1, 
+			const std::vector<InitCondition<T>>& approximation_h2, const T h2, const T alpha, 
+			const T percentToleranceFunc, const T percentToleranceDeriv)
+		{
+			 T maxValueError_h1, maxDerivError_h1;
+
+			 GetMaxValueAndDerivativeErrors(approximation_h1, alpha, maxValueError_h1, maxDerivError_h1);
+
+			 T maxValueError_h2, maxDerivError_h2;
+
+			 GetMaxValueAndDerivativeErrors(approximation_h2, alpha, maxValueError_h2, maxDerivError_h2);
+
+			 T valueRatio = maxValueError_h2/maxValueError_h1;
+			 T derivativeRatio = maxDerivError_h2/maxDerivError_h1;
+
+			 T h2_to_h1_ratio_squared = auxutils::sqr(h2/h1);
+
+			 Assert::IsTrue(valueRatio >= h2_to_h1_ratio_squared*(1 - percentToleranceFunc), Message("Too low ratio for values: " + auxutils::ToString(valueRatio)));
+			 Assert::IsTrue(derivativeRatio >= h2_to_h1_ratio_squared*(1 - percentToleranceDeriv), Message("Too low ratio for derivatives: " + auxutils::ToString(derivativeRatio)));
 		}
 
 	public:
@@ -111,46 +178,44 @@ namespace GeneralTest
 			 double h1 = 0.0001;
 			 double alpha = 2;
 			 double h2 = h1*10;
-			 auto result_h1 = GetGybridShootingResultForward(alpha, h1);
-			 auto result_h2 = GetGybridShootingResultForward(alpha, h2);
+			 auto result_h1 = GetGybridShootingResultForward(alpha, h1, false);
+			 auto result_h2 = GetGybridShootingResultForward(alpha, h2, false);
 
-			 double maxValueError_h1, maxDerivError_h1;
-
-			 GetMaxValueAndDerivativeErrors(result_h1, alpha, maxValueError_h1, maxDerivError_h1);
-
-			 double maxValueError_h2, maxDerivError_h2;
-
-			 GetMaxValueAndDerivativeErrors(result_h2, alpha, maxValueError_h2, maxDerivError_h2);
-
-			 double valueRatio = maxValueError_h2/maxValueError_h1;
-			 double derivativeRatio = maxDerivError_h2/maxDerivError_h1;
-
-
-			 Assert::IsTrue(valueRatio >= auxutils::sqr(h2/h1)*(1 - 0.002), Message("Too low ratio for values: " + auxutils::ToString(valueRatio)));
-			 Assert::IsTrue(derivativeRatio >= auxutils::sqr(h2/h1)*(1 - 0.002), Message("Too low ratio for derivatives: " + auxutils::ToString(derivativeRatio)));
+			 AssertQuadraticOrderOfApproximation(result_h1, h1, result_h2, h2, alpha, 0.002, 0.002);
 		}
+
+		TEST_METHOD(NonAutoNonUniformHybridShootingTest)
+		{
+			 double h1 = 0.0001;
+			 double alpha = 2;
+			 double h2 = h1*10;
+			 auto result_h1 = GetGybridShootingResultForward(alpha, h1, true);
+			 auto result_h2 = GetGybridShootingResultForward(alpha, h2, true);
+
+			 AssertQuadraticOrderOfApproximation(result_h1, h1, result_h2, h2, alpha, 0.001, 0.001);
+		}
+
 
 		TEST_METHOD(NonUniformHybridShootingBackwardTest)
 		{
 			 double h1 = -0.0001;
 			 double h2 = 10*h1;
 			 double alpha = 2;
-			 auto result_h1 = GetGybridShootingResultBackward(alpha, h1);
-			 auto result_h2 = GetGybridShootingResultBackward(alpha, h2);
+			 auto result_h1 = GetGybridShootingResultBackward(alpha, h1, false);
+			 auto result_h2 = GetGybridShootingResultBackward(alpha, h2, false);
 
-			 double maxValueError_h1, maxDerivError_h1;
+			 AssertQuadraticOrderOfApproximation(result_h1, h1, result_h2, h2, alpha, 0.0, 0.0);
+		}
 
-			 GetMaxValueAndDerivativeErrors(result_h1, alpha, maxValueError_h1, maxDerivError_h1);
+		TEST_METHOD(NonAutoNonUniformHybridShootingBackwardTest)
+		{
+			 double h1 = -0.0001;
+			 double h2 = 10*h1;
+			 double alpha = 2;
+			 auto result_h1 = GetGybridShootingResultBackward(alpha, h1, true);
+			 auto result_h2 = GetGybridShootingResultBackward(alpha, h2, true);
 
-			 double maxValueError_h2, maxDerivError_h2;
-
-			 GetMaxValueAndDerivativeErrors(result_h2, alpha, maxValueError_h2, maxDerivError_h2);
-
-			 double valueRatio = maxValueError_h2/maxValueError_h1;
-			 double derivativeRatio = maxDerivError_h2/maxDerivError_h1;
-
-			 Assert::IsTrue(valueRatio >= auxutils::sqr(h2/h1), Message("Too low ratio for values: " + auxutils::ToString(valueRatio)));
-			 Assert::IsTrue(derivativeRatio >= auxutils::sqr(h2/h1), Message("Too low ratio for derivatives: " + auxutils::ToString(derivativeRatio)));
+			 AssertQuadraticOrderOfApproximation(result_h1, h1, result_h2, h2, alpha, 0.0, 0.009);
 		}
 
 		TEST_METHOD(NonUniformHybridBisectionTest)
@@ -159,62 +224,50 @@ namespace GeneralTest
 			 double alpha = 2;
 			 double h2 = h1*10;
 
-			 auto result_h1 = GetHybridBisectionResult(alpha, h1);
-			 auto result_h2 = GetHybridBisectionResult(alpha, h2);
+			 auto result_h1 = GetHybridBisectionResult(alpha, h1, false);
+			 auto result_h2 = GetHybridBisectionResult(alpha, h2, false);
 
-			 double maxValueError_h1, maxDerivError_h1;
+			 AssertQuadraticOrderOfApproximation(result_h1, h1, result_h2, h2, alpha, 0.0, 0.002);
+		}
 
-			 GetMaxValueAndDerivativeErrors(result_h1, alpha, maxValueError_h1, maxDerivError_h1);
+		TEST_METHOD(NonAutoNonUniformHybridBisectionTest)
+		{
+			 double h1 = 0.0001;
+			 double alpha = 2;
+			 double h2 = h1*10;
 
-			 double maxValueError_h2, maxDerivError_h2;
+			 auto result_h1 = GetHybridBisectionResult(alpha, h1, true);
+			 auto result_h2 = GetHybridBisectionResult(alpha, h2, true);
 
-			 GetMaxValueAndDerivativeErrors(result_h2, alpha, maxValueError_h2, maxDerivError_h2);
-
-			 double valueRatio = maxValueError_h2/maxValueError_h1;
-			 double derivativeRatio = maxDerivError_h2/maxDerivError_h1;
-
-			 Assert::IsTrue(valueRatio >= auxutils::sqr(h2/h1), Message("Too low ratio for values: " + auxutils::ToString(valueRatio)));
-			 Assert::IsTrue(derivativeRatio >= auxutils::sqr(h2/h1)*(1-0.002), Message("Too low ratio for derivatives: " + auxutils::ToString(derivativeRatio)));
+			 AssertQuadraticOrderOfApproximation(result_h1, h1, result_h2, h2, alpha, 0.0, 0.0003);
 		}
 
 		TEST_METHOD(NonUniformHybridMultipleShootingTest)
 		{
+			 auto init_guess = auxutils::ReadFromFile<InitCondition<numTypeMp>>("TestData\\NonUniformProblemInitGuess.txt");
+
 			 numTypeMp alpha = numTypeMp(2)/1;
-
-			 auto init_guess = auxutils::ReadFromFile<InitCondition<numTypeMp>>("f:\\NonUniformAutonomousProblemInitGuess.txt");
-
-			 AutonomousNonUniformProblem<numTypeMp> problem(alpha);
-			 HybridMultipleShootingComponent<numTypeMp> HMSComp(problem);
-
-			 bool succeeded;
 			 numTypeMp h1 = numTypeMp(1)/10000;
-			 std::vector<InitCondition<numTypeMp>> result_h1 = HMSComp.Run(init_guess, h1, succeeded);
-
-			 Assert::IsTrue(succeeded, Message("Hybrid multiple shooting failed for h1"));
-
-			 Assert::IsTrue(CheckQuadraticConvergenceOfNewtonMethd(HMSComp.GetCorrectionMgnitudes()), Message("Cannot confirm quadratic convergence rate"));
-
 			 numTypeMp h2 = 10*h1;
-			 std::vector<InitCondition<numTypeMp>> result_h2 = HMSComp.Run(init_guess, h2, succeeded);
 
-			 Assert::IsTrue(succeeded, Message("Hybrid multiple shooting failed for h2"));
+			 auto result_h1 = GetHybridMultipleShootingResult(alpha, h1, init_guess, false); 
+			 auto result_h2 = GetHybridMultipleShootingResult(alpha, h2, init_guess, false); 
 
-			 Assert::IsTrue(CheckQuadraticConvergenceOfNewtonMethd(HMSComp.GetCorrectionMgnitudes()), Message("Cannot confirm quadratic convergence rate"));
-
-			 numTypeMp maxValueError_h1, maxDerivError_h1;
-
-			 GetMaxValueAndDerivativeErrors(result_h1, alpha, maxValueError_h1, maxDerivError_h1);
-
-			 numTypeMp maxValueError_h2, maxDerivError_h2;
-
-			 GetMaxValueAndDerivativeErrors(result_h2, alpha, maxValueError_h2, maxDerivError_h2);
-
-			 numTypeMp valueRatio = maxValueError_h2/maxValueError_h1;
-			 numTypeMp derivativeRatio = maxDerivError_h2/maxDerivError_h1;
-
-			 Assert::IsTrue(valueRatio >= auxutils::sqr(h2/h1)*(1-0.012), Message("Too low ratio for values: " + auxutils::ToString(valueRatio)));
-			 Assert::IsTrue(derivativeRatio >= auxutils::sqr(h2/h1)*(1-0.012), Message("Too low ratio for derivatives: " + auxutils::ToString(derivativeRatio)));
+			 AssertQuadraticOrderOfApproximation<numTypeMp>(result_h1, h1, result_h2, h2, alpha, 0.012, 0.012);
     	}
 
+		TEST_METHOD(NonAutoNonUniformHybridMultipleShootingTest)
+		{
+			 auto init_guess = auxutils::ReadFromFile<InitCondition<numTypeMp>>("TestData\\NonUniformProblemInitGuess.txt");
+
+			 numTypeMp alpha = numTypeMp(2)/1;
+			 numTypeMp h1 = numTypeMp(1)/5000;
+			 numTypeMp h2 = 10*h1;
+
+			 auto result_h1 = GetHybridMultipleShootingResult(alpha, h1, init_guess, true); 
+			 auto result_h2 = GetHybridMultipleShootingResult(alpha, h2, init_guess, true); 
+
+			 AssertQuadraticOrderOfApproximation(result_h1, h1, result_h2, h2, alpha, 0.0, 0.0);
+    	}
 	};
 }
